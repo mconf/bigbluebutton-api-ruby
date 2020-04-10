03fb5dd925f38ec31bd499a4793db9a82f4bc7b2
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-04-10 11:47:01 -0300

    Fix deprecated TimeoutError constant

--- lib/bigbluebutton_api.rb
+++ lib/bigbluebutton_api.rb
@@ -731,7 +731,7 @@ module BigBlueButton
         end
         puts "BigBlueButtonAPI: URL response = #{response.body}" if @debug
 
-      rescue TimeoutError => error
+      rescue Timeout::Error => error
         raise BigBlueButtonException.new("Timeout error. Your server is probably down: \"#{@url}\". Error: #{error}")
 
       rescue Exception => error
