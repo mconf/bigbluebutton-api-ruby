583c538a635ef04736ce611d399eae98dc626734
JoaoCarAlmSilva <joao.almeidaomm@gmail.com>
2021-11-18 14:26:42 -0300

    Change parameters to initialize from BigBlueButtonApi

--- lib/bigbluebutton_api.rb
+++ lib/bigbluebutton_api.rb
@@ -51,9 +51,6 @@ module BigBlueButton
     # API version e.g. 0.81
     attr_accessor :version
 
-    # Flag to turn on/off debug mode
-    attr_accessor :debug
-
     # logger to log reponses and infos
     attr_accessor :logger
 
@@ -71,26 +68,25 @@ module BigBlueButton
     # url::       URL to a BigBlueButton server (e.g. http://demo.bigbluebutton.org/bigbluebutton/api)
     # secret::    Shared secret for this server
     # version::   API version e.g. 0.81
-    def initialize(url, secret, version=nil, debug=false)
+    def initialize(url, secret, version=nil, logger=nil)
       @supported_versions = ['0.8', '0.81', '0.9', '1.0']
       @url = url
       @secret = secret
-      @debug = debug
       @timeout = 10         # default timeout for api requests
       @request_headers = {} # http headers sent in all requests
-
-      @logger = Logger.new(STDOUT)
-      @logger.level = Logger::DEBUG
-
+      @logger = logger
+      if logger.nil?
+        @logger = Logger.new(STDOUT)
+        @logger.level = Logger::INFO
+      end
+      
       version = nil if version && version.strip.empty?
       @version = nearest_version(version || get_api_version)
       unless @supported_versions.include?(@version)
         @logger.warn("BigBlueButtonAPI: detected unsupported version, using the closest one that is supported (#{@version})")
-        # puts "BigBlueButtonAPI: detected unsupported version, using the closest one that is supported (#{@version})"
       end
 
       @logger.debug("BigBlueButtonAPI: Using version #{@version}")
-      # puts "BigBlueButtonAPI: Using version #{@version}" if @debug
     end
 
     # Creates a new meeting. Returns the hash with the response or throws BigBlueButtonException
@@ -645,7 +641,7 @@ module BigBlueButton
     # API's are equal if all the following attributes are equal.
     def ==(other)
       r = true
-      [:url, :supported_versions, :secret, :version, :debug].each do |param|
+      [:url, :supported_versions, :secret, :version, :logger].each do |param|
         r = r && self.send(param) == other.send(param)
       end
       r
@@ -753,7 +749,6 @@ module BigBlueButton
     def send_request(url, data=nil)
       begin
         @logger.debug("BigBlueButtonAPI: URL request = #{url}")
-        # puts "BigBlueButtonAPI: URL request = #{url}" if @debug
         url_parsed = URI.parse(url)
         http = Net::HTTP.new(url_parsed.host, url_parsed.port)
         http.open_timeout = @timeout
@@ -764,13 +759,11 @@ module BigBlueButton
           response = http.get(url_parsed.request_uri, @request_headers)
         else
           @logger.debug("BigBlueButtonAPI: Sending as a POST request with data.size = #{data.size}")
-          # puts "BigBlueButtonAPI: Sending as a POST request with data.size = #{data.size}" if @debug
           opts = { 'Content-Type' => 'application/xml' }.merge @request_headers
           response = http.post(url_parsed.request_uri, data, opts)
         end
-        # @logger.debug("BigBlueButtonAPI: URL response = #{response.body}")
         @logger.info("BigBlueButtonAPI: request=#{url} response_status=#{response.class.name} response_code=#{response.code}  message_key=#{response.message}")
-        # puts "BigBlueButtonAPI: URL response = #{response.body}" if @debug
+        @logger.debug("BigBlueButtonAPI: URL response = #{response.body}")
 
       rescue Timeout::Error => error
         raise BigBlueButtonException.new("Timeout error. Your server is probably down: \"#{@url}\". Error: #{error}")
