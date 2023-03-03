9e8d6e8820241795720fc39c68974dede7e571ee
farhatahmad <ahmad.af.farhat@gmail.com>
2023-03-03 10:09:02 -0500

    Prevent double slash on api calls

--- lib/bigbluebutton_api.rb
+++ lib/bigbluebutton_api.rb
@@ -691,7 +691,9 @@ module BigBlueButton
         params_string = "checksum=#{checksum}&#{params_string}"
         return "#{@url}/#{method}", params_string
       else
-        url = "#{@url}/#{method}?"
+        url = @url
+        url += "/" unless url.end_with?("/")
+        url += method
         url += "#{params_string}&" unless params_string.empty?
         url += "checksum=#{checksum}"
         return url, nil
